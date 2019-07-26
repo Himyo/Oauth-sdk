62653ab3405200080219968fb4e1fefd4767b7c8
Tom Délié <tomdelie78@gmail.com>
2019-07-26 23:32:36 +0200

    style and chatbot link

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require('./GenericProvider.php');
 $providerName = trim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
 
@@ -8,8 +7,22 @@ $json = file_get_contents('./conf.json');
 $conf = json_decode($json, true);
 
 if($providerName == '') {
-    echo '<a href="/facebook?provider=authorization">Facebook</a><br />';
-    echo '<a href="/github?provider=authorization">Github</a><br />';
+    ?>
+    <div class="mx-auto mt-4" style="padding: 1rem; background-color: #eeeeee; border: 1px solid #bbbbbb    ; border-radius: 4px; display: flex; flex-direction: column; align-items: center; width: 300px;">
+        <div>
+            <h1>SDK</h1>
+            <hr>
+        </div>
+
+        <div class="mt-2">
+            <a class="btn btn-info mr-2" href="/facebook?provider=authorization">Facebook</a>
+            <a class="btn btn-success" href="/github?provider=authorization">Github</a>
+        </div>
+
+        <a href="https://m.me/2282859145292393" target="_blank" class="mt-3 btn btn-danger">CHATBOT</a>
+
+    </div>
+<?php
 }
 
 if(isset($conf[$providerName])){
@@ -43,3 +56,5 @@ if($_GET['code']) {
         echo "Error with access_token request";
     }
 }
+?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
